5a4967076d9be219aeb85f82bfe86cf8b458e825
izabela08 <izabelalima17@gmail.com>
2022-08-16 18:14:05 -0300

    a15-templates

---
+++ script.js
@@ -0,0 +1,46 @@
+// let num = Number(prompt('Digite um numero, digite zero para sair. '))
+// let soma = 0
+
+// while(num !==0){
+//     soma = soma + num
+//     num = Number(prompt('Digite um número. Para sair digite ZERO'))
+
+// }
+
+// alert('A soma dos número é,' + soma)
+
+// let num = Number(prompt('Digite um número'))
+
+// for(let i = 0; i <= num; i++){
+//     console.log(i)
+// }
+
+// const array= [7,6,5,4,3,2,1]
+
+// for(let i = 0; i <= array.length; i++){
+//     console.log('o número do índice é ' + i + ' é ' + array[i])
+// }
+
+// EXERCICIO 1
+// let tipo = prompt('Digite seu tipo de usuario: [A] [B] [C]').toUpperCase()
+
+// while(tipo !== "A"){
+//     alert('Acesso negado')
+//     tipo = prompt('Digite seu tipo de usuario: [A] [B] [C]').toUpperCase()
+
+// }
+// alert('bem vindo adm')
+
+// EXERCICIO 2
+
+// for(let i = 0; i <= 20; i++){
+//     console.log(i++)
+// }
+
+// EXERCICIO 3
+
+const array = ['iza', 'anna', 'bia', 'leo' , 'pedro']
+
+for(let i = 0; i < array.length; i++ ){
+    console.log(array[i])
+}
